8f1165c54a2b59454a1125e076d46467e3a79dba
Andrey Skopintsev <sanchezzvilapandozz@gmail.com>
2021-10-26 12:56:49 +0300

    added different handlers for each operation and a calculation function that handles only operations

--- firstapp/Task.go
+++ firstapp/Task.go
@@ -7,6 +7,11 @@ import (
 	"strconv"
 )
 
+const AddOperator int = 0
+const SubOperator int = 1
+const MulOperator int = 2
+const DivOperator int = 3
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home")
 	fmt.Println("Endpoint hit: homepage")
@@ -14,18 +19,21 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/api/add", calculation)
-	http.HandleFunc("/api/sub", calculation)
-	http.HandleFunc("/api/mul", calculation)
-	http.HandleFunc("/api/div", calculation)
+	http.HandleFunc("/api/add", addNumbers)
+	http.HandleFunc("/api/sub", subNumbers)
+	http.HandleFunc("/api/mul", mulNumbers)
+	http.HandleFunc("/api/div", divNumbers)
 
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
-func calculation(w http.ResponseWriter, r *http.Request) {
-	u := r.URL.Path
+// Handlers for operations
+// Addition
+
+func addNumbers(w http.ResponseWriter, r *http.Request) {
 	var first int
 	var second int
+	var result int
 
 	if a, err := strconv.Atoi(r.URL.Query().Get("a")); err == nil {
 		first = a
@@ -39,17 +47,106 @@ func calculation(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "There was an error: %d", err)
 	}
 
-	switch u {
-	case "/api/add":
-		fmt.Fprintf(w, "The sum is %d", first+second)
-	case "/api/sub":
-		fmt.Fprintf(w, "Subtraction result is %d", first-second)
-	case "/api/mul":
-		fmt.Fprintf(w, "Multiplication result is %d", first*second)
-	case "/api/div":
-		fmt.Fprintf(w, "Division result is %d", first/second)
+	result = calculation(first, second, AddOperator)
+
+	fmt.Fprintf(w, "Addition result is %d", result)
+
+}
+
+// Subtraction
+
+func subNumbers(w http.ResponseWriter, r *http.Request) {
+	var first int
+	var second int
+	var result int
+
+	if a, err := strconv.Atoi(r.URL.Query().Get("a")); err == nil {
+		first = a
+	} else {
+		fmt.Fprintf(w, "There was an error: %d", err)
 	}
 
+	if b, err := strconv.Atoi(r.URL.Query().Get("b")); err == nil {
+		second = b
+	} else {
+		fmt.Fprintf(w, "There was an error: %d", err)
+	}
+
+	result = calculation(first, second, SubOperator)
+
+	fmt.Fprintf(w, "Addition result is %d", result)
+
+}
+
+// Multiplication
+
+func mulNumbers(w http.ResponseWriter, r *http.Request) {
+	var first int
+	var second int
+	var result int
+
+	if a, err := strconv.Atoi(r.URL.Query().Get("a")); err == nil {
+		first = a
+	} else {
+		fmt.Fprintf(w, "There was an error: %d", err)
+	}
+
+	if b, err := strconv.Atoi(r.URL.Query().Get("b")); err == nil {
+		second = b
+	} else {
+		fmt.Fprintf(w, "There was an error: %d", err)
+	}
+
+	result = calculation(first, second, MulOperator)
+
+	fmt.Fprintf(w, "Multiplication result is %d", result)
+
+}
+
+// Division
+
+func divNumbers(w http.ResponseWriter, r *http.Request) {
+	var first int
+	var second int
+	var result int
+
+	if a, err := strconv.Atoi(r.URL.Query().Get("a")); err == nil {
+		first = a
+	} else {
+		fmt.Fprintf(w, "There was an error: %d", err)
+	}
+
+	if b, err := strconv.Atoi(r.URL.Query().Get("b")); err == nil {
+		second = b
+	} else {
+		fmt.Fprintf(w, "There was an error: %d", err)
+	}
+
+	if second != 0 {
+		result = calculation(first, second, DivOperator)
+		fmt.Fprintf(w, "Division result is %d", result)
+	} else {
+		fmt.Fprintf(w, "You can't divide by zero")
+	}
+}
+
+// Calculation fucntion which handles all the operation based on values queried from URL
+//  and an operator constant that's being passed
+func calculation(a int, b int, oper int) int {
+	var result int
+
+	switch oper {
+	case 0:
+		result = a + b
+	case 1:
+		result = a - b
+	case 2:
+		result = a * b
+	case 3:
+		result = a / b
+	}
+
+	return result
 }
 
 func main() {
